daf7e4ab8b9e3c629fee2a8b16ba4569f532dd4f
MSilva95 <miguel-silva98@hotmail.com>
2020-04-09 16:39:26 +0100

    new exercise for regex using non capturing groups

--- tests/group-price/pass_ms.js
+++ tests/group-price/pass_ms.js
@@ -1,12 +1,15 @@
 const groupPrice = (str) => {
-  let arr = []
-  var myRegexp = /(?:\$|(?:USD))([0-9]+)\.([0-9]{2})/g
-  var match = myRegexp.exec(str)
- if (match != null) {
+  let myregex = /(?:\$|(?:USD))([0-9]+)\.([0-9]{2})/g
+  let match = myregex.exec(str)
+  let res = []
+
+  while (match) {
+    let arr = []
     arr.push(match[0])
     arr.push(match[1])
     arr.push(match[2])
-    return arr
+    res.push(arr)
+    match = myregex.exec(str)
   }
-  return arr
+  return res
 }
